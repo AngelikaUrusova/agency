b2aa16c4e5b920c9e4ed4b8ba057de0677ac8083
AngelikaUrusova <129684308+AngelikaUrusova@users.noreply.github.com>
2023-07-06 09:01:09 +0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -4,15 +4,3 @@ const menuEl = document.querySelector('.burger__menu');
 burgerEl.addEventListener('click', function () {
   menuEl.classList.toggle('menu_show');
 });
-
-
-const nameBgEL = document.querySelector('.name__bg')
-const photoEL = document.querySelector('.photo')
-const personsName = document.querySelector('.persons__name')
-
-
-photoEL.addEventListener('click', function () {
-  nameBgEL.classList.toggle('active');
-  personsName.classList.toggle('active');
-});
-
